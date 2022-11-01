e47264662187eb825fb233d625fa29cf000ca33b
shclgus112 <shclgus112@gmail.com>
2022-11-01 20:14:36 +0900

    https://github.com/JuniorTunarr/diary.git

---
+++ screens/CalendarScreen.js
@@ -0,0 +1,38 @@
+import {format} from 'date-fns';
+import React, {useContext, useMemo, useState} from 'react';
+import CalendarView from '../components/CalendarView';
+import FeedList from '../components/FeedList';
+import LogContext from '../contexts/LogContext';
+
+function CalendarScreen() {
+  const {logs} = useContext(LogContext);
+  const [selectedDate, setSelectedDate] = useState(
+    format(new Date(), 'yyyy-MM-dd'),
+  );
+  const filteredLogs = logs.filter(
+    log => format(new Date(log.date), 'yyyy-MM-dd') === selectedDate,
+  );
+  const markedDates = useMemo(
+    () =>
+      logs.reduce((acc, current) => {
+        const formattedDate = format(new Date(current.date), 'yyyy-MM-dd');
+        acc[formattedDate] = {marked: true};
+        return acc;
+      }, {}),
+    [logs],
+  );
+  return (
+    <FeedList
+      logs={filteredLogs}
+      ListHeaderComponent={
+        <CalendarView
+          markedDates={markedDates}
+          selectedDate={selectedDate}
+          onSelectDate={setSelectedDate}
+        />
+      }
+    />
+  );
+}
+
+export default CalendarScreen;
